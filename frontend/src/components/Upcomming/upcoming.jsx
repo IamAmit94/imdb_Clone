484989ea9cf61e69c870af333aa39c133e186079
IamAmit94 <amitrvt18@hotmail.com>
2024-01-26 23:20:15 +0530

    first commit

---
+++ frontend/src/components/Upcomming/upcoming.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./styles.module.css";
+
+const upcoming = () => {
+  return (
+    <div>
+        <button
+        className={styles.btn}>Upcoming Movies</button>
+    </div>
+  )
+}
+
+export default upcoming
